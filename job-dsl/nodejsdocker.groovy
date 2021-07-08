4898eaade3f2f8744fbdb2c87d79bec3b47953f2
aakopianlz <87097892+aakopianlz@users.noreply.github.com>
2021-07-08 12:06:40 -0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/aakopianlz/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/aakopianlz/jenkins-docker-tutorial.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
